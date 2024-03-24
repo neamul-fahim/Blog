7c37d26eeca8d8a3c902c84b1cd465adfd73f899
neamul fahim <102896638+neamul-fahim@users.noreply.github.com>
2024-03-24 13:01:03 +0600

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'neamul.fahimb@gmail.com'
-EMAIL_HOST_PASSWORD = 'bwhdfyyuyrrfdrxp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 ALLOWED_HOSTS = ["*"]
 BASE_API_URL = "http://127.0.0.1:8000"
